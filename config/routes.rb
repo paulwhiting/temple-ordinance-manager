b288ca88b0bdcdd99cbcec3bf248139f665a448e
Paul Whiting <paulwhiting@gmail.com>
2017-01-16 15:10:42 +0000

    updates to get production environment working in a subdirectory

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'portal#index'
-  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/familysearch', as: 'login'
+  get '/auth/familysearch/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-	#get '/print/:person_id/:ordinance', to: 'portal#print', as: :print
-	resources :comments
-	resources :contacts
-	resources :assignments do
+  #get '/print/:person_id/:ordinance', to: 'portal#print', as: :print
+  resources :comments
+  resources :contacts
+  resources :assignments do
     collection do
       get 'print/:id', action: 'print', as: 'print' # return a pdf of the assignments
     end
