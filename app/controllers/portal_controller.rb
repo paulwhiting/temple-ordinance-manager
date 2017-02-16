3840a14ac0910ea680955449667f934405f27ed0
Paul Whiting <paulwhiting@gmail.com>
2017-02-15 20:18:57 -0500

    code to debug reservation responses

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -1,17 +1,29 @@
 class PortalController < ApplicationController
   skip_before_action :require_login, :only => [:index]
 
-	def index
+  def index
     if current_user
-			@people = []
-			begin
-				response = current_user.client.get("/platform/reservations/users/#{current_user.uid}")
-				@people = Person.from_response( current_user, response )
-			rescue FamilySearch::Error::ClientError
-				#people = []
-			end
+      @people = []
+      begin
+        response = current_user.client.get("/platform/reservations/users/#{current_user.uid}")
+        @people = Person.from_response( current_user, response )
+      rescue FamilySearch::Error::ClientError
+        #people = []
+      end
       @people.sort! { |a,b| a.name.downcase <=> b.name.downcase }
     end
-	end
+  end
+
+  def debug
+    if current_user
+      begin
+        response = current_user.client.get("/platform/reservations/users/#{current_user.uid}")
+        render plain: response.to_yaml
+      rescue FamilySearch::Error::ClientError
+        response = "An error happened.  That's all I know."
+        render plain: response
+      end
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'portal#index'
+  get '/debug', to: 'portal#debug'
   get '/auth/familysearch', as: 'login'
   get '/auth/familysearch/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  #get '/print/:person_id/:ordinance', to: 'portal#print', as: :print
   resources :comments
   resources :contacts
   resources :assignments do
